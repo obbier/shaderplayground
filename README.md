a3dda4c1779f148832094f9dd7e95f7a36a0553c
Obbie Richardson <ob@obbier.com>
2014-12-27 15:19:13 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+Shader playground for rapid prototyping.
---
+++ js/site.js
@@ -0,0 +1,8 @@
+
+var settingsViewModel = {
+  canvasHeight: ko.observable(500),
+  canvasWidth: ko.observable(500)
+    
+};
+
+ko.applyBindings( settingsViewModel );
